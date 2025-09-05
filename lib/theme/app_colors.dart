88b3e885ca44676fd5b98f12a2a9bcf9581483dc
Wallace Santana <wallacesantanak0@gmail.com>
2025-09-05 18:38:14 -0300

    feat(app_colors): create class with colors using in aplication

---
+++ lib/theme/app_colors.dart
@@ -0,0 +1,46 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  /* 
+    Botões principais (CTA): Verde (#2E7D32).
+   Destaques (filtros, ícones de necessidade): Laranja (#FF9800).
+   Background da UI: Cinza claro (#F5F5F5).
+   Texto principal: Preto (#212121).
+   Texto secundário: Cinza médio (#616161).
+   Colors:
+      green-500 =  #2E7D32
+      green-400= #94D297
+      green-300 = #DCFCDD
+
+      blue-400=#1B4B51
+      blue-500=#0095A6
+      blue-600=#1A3033
+
+      orange-500=#FF9800
+      orange-600=#AA7D39
+      orange-650=#806640
+      
+      clean-cian=#F5F5F5
+      median-cian=#616161
+      black=#212121
+      error=#B00020FF
+  */
+
+  static const Color green_500 = Color(0xFF2E7D32);
+  static const Color green_400 = Color(0xFF94D297);
+  static const Color green_300 = Color(0xFFDCFCDD);
+
+  static const Color blue_400 = Color(0xFF1B4B51);
+  static const Color blue_500 = Color(0xFF0095A6);
+  static const Color blue_600 = Color(0xFF1A3033);
+
+  static const Color orange_500 = Color(0xFFFF9800);
+  static const Color orange_600 = Color(0xFFAA7D39);
+  static const Color orange_650 = Color(0xFF806640);
+
+  static const Color cleanCian = Color(0xFFF5F5F5);
+  static const Color medianCian = Color(0xFF616161);
+  static const Color black = Color(0xFF212121);
+
+  static const Color error = Color(0xFFB00020);
+}
